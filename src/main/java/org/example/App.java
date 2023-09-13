ff69a28aeaee0eaaab0179656d26a3dc86264e1d
ludwigpersson <perssonludwig1@gmail.com>
2023-09-13 12:22:08 +0200

    Allt för G betyg

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,8 +1,12 @@
 package org.example;
+import java.text.DecimalFormat;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class App {
+    private static final DecimalFormat df = new DecimalFormat("##");
     public static void main(String[] args) {
+        new Locale("sv","SE");
         Scanner input = new Scanner(System.in);
         boolean running = true;
         int[] prices = new int[24];
@@ -19,13 +23,11 @@ public class App {
 
             String choice = input.next();
 
-            //sorteraa
-            //bästa laddtid
             switch (choice.toLowerCase()) {
                 case "1" -> inputPrices(prices, input);
                 case "2" -> printMinAndMaxPrices(prices);
-                case "3" -> System.out.println("sortera");
-                case "4" -> System.out.println("bästa laddtid");
+                case "3" -> sorting(prices);
+                case "4" -> findBestChargingTime(prices);
                 case "e" -> {
                     System.out.println("Programmet avslutas.");
                     running = false;
@@ -34,7 +36,7 @@ public class App {
             }
         }
 
-        input.close();
+        //input.close();
     }
 
     public static void inputPrices(int[] prices, Scanner input) {
@@ -76,7 +78,75 @@ public class App {
         System.out.println("Högsta pris: " + tim(maxHour) + "-" + tim(maxHour + 1) + ", " + maxPrice + " öre/kWh");
         System.out.println("Medelpris: " + String.format("%.2f", averagePrice) + " öre/kWh");
     }
-    static String tim (int timme) {
+
+    public static void sorting(int[] prices) {
+        float[] pricePerHour = new float[24];
+        int[] time = new int[24];
+
+        for (int i = 0; i < 24; i++) {
+            pricePerHour[i]=prices[i];
+            time[i] = i;
+        }
+
+        boolean sorted = false;
+        while (!sorted) {
+
+            sorted = true;
+            for (int i = 0; i < 23; i++) {
+                float t1 = pricePerHour[i];
+                float t2 = pricePerHour[i + 1];
+
+                int k1 = time[i];
+                int k2 = time[i + 1];
+
+                if (t1 < t2) {
+                    pricePerHour[i] = t2;
+                    pricePerHour[i + 1] = t1;
+
+                    time[i] = k2;
+                    time[i + 1] = k1;
+
+                    sorted = false;
+
+                }
+
+            }
+
+        }
+
+        System.out.println();
+        for (int i = 0; i < 24; i++) {
+            int hour = time[i];
+            int nextHour = time[i] + 1;
+            String formattedPrice = df.format(pricePerHour[i]);
+            System.out.println(tim(hour) + "-" + tim(nextHour) + " " + formattedPrice + " öre");
+
+            
+            
+        }    
+    }
+    public static void findBestChargingTime(int[] prices) {
+        int lowestTotalPrice = Integer.MAX_VALUE;
+        int bestStartTime = -1;
+
+        for (int startTime = 0; startTime < 21; startTime++) {
+            int totalPrice = 0;
+            for (int i = startTime; i < startTime + 4; i++) {
+                totalPrice += prices[i];
+            }
+            if (totalPrice < lowestTotalPrice) {
+                lowestTotalPrice = totalPrice;
+                bestStartTime = startTime;
+            }
+        }
+
+        double averageValue = (double) lowestTotalPrice / 4;
+
+        System.out.printf(String.format("Påbörja laddning klockan %02d\n", bestStartTime, + 3));
+        System.out.printf(String.format("Medelpris 4h: %.1f öre/kWh\n",averageValue));
+    }
+
+    static String tim(int timme) {
         String tid;
         if (timme < 10) {
             tid = "0" + timme;
@@ -85,5 +155,4 @@ public class App {
         }
         return tid;
     }
-
 }
